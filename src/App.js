ed7d68a2c16fc4f8579f50dda2be304481cf8095
revas12 <49127445+revas12@users.noreply.github.com>
2021-06-05 11:44:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
 
 function App() {
   return (
